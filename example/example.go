294f3036e6bbda60d3c41457947c515993c04251
Daanshaw <74239386+Daanshaw@users.noreply.github.com>
2023-03-23 11:40:03 +0000

    added user input for threshold and number of parties

--- example/example.go
+++ example/example.go
@@ -162,11 +162,32 @@ func All(id party.ID, ids party.IDSlice, threshold int, message []byte, n *test.
 }
 
 func main() {
+	var numParties, threshold int
+	fmt.Print("Enter the number of parties: ")
+	_, err := fmt.Scanln(&numParties)
+	if err != nil {
+		fmt.Println("Invalid input for the number of parties")
+		return
+	}
 
-	ids := party.IDSlice{"a", "b", "c", "d", "e", "f"}
-	threshold := 4
+	fmt.Print("Enter the threshold: ")
+	_, err = fmt.Scanln(&threshold)
+	if err != nil {
+		fmt.Println("Invalid input for the threshold")
+		return
+	}
+
+	if threshold >= numParties {
+		fmt.Println("Threshold must be less than the number of parties")
+		return
+	}
+
+	ids := make(party.IDSlice, numParties)
+	for i := 0; i < numParties; i++ {
+		ids[i] = party.ID(fmt.Sprintf("p%d", i+1))
+	}
+
 	messageToSign := []byte("hello")
-
 	net := test.NewNetwork(ids)
 
 	var wg sync.WaitGroup
